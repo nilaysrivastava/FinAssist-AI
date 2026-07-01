19e8581f19b6c4f623d0aa4566373785b41ef177
Nilay Srivastava <nilaysrivastava2003@gmail.com>
2026-07-01 17:20:42 +0530

    added deployed frontend url to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,9 +15,13 @@ from app.email_service import create_email_offer, send_email_offer
 
 app = FastAPI(title="FinAssist GenAI Chatbot", version="2.0.0")
 
+https://finassist-ai-six.vercel.app/
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://finassist-ai-six.vercel.app/",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
